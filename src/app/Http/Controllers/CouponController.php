4392d26450f9e93f5853b8ae2c8350427160234f
KirMaid <ialavash228@gmail.com>
2024-01-11 12:24:31 +0300

    Добавил тест на открытие страницы регистрации

--- src/app/Http/Controllers/CouponController.php
+++ src/app/Http/Controllers/CouponController.php
@@ -4,21 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Coupon;
+use App\Models;
+
 
 class CouponController extends Controller
 {
     public function apply(Request $request)
     {
-        $coupon = Coupon::where('code', $request->coupon)->first();
+        $coupon = Models\Coupon::where('code', $request->coupon)->first();
 
         if (!$coupon) {
             return redirect()->route('index')->withErrors('Такого купона не существует');
         }
 
-
         $request->session()->put('coupon', [
             'code' => $coupon->code,
-            #'discount' => $coupon->discount
+            'discount' => $coupon->discount
         ]);
 
         return redirect()->route('index')->with('success_message', 'Купон применён');
