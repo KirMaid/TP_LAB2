6b9e15fcafcfc2e5963e49fdb24580d85335fa7b
KirMaid <ialavash228@gmail.com>
2024-01-11 10:57:12 +0300

    добавил несколько новых тестов

--- src/tests/Feature/ProductRequestTest.php
+++ src/tests/Feature/ProductRequestTest.php
@@ -2,11 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use App\Models\User;
 use Carbon\Carbon;
-use Database\Factories\UserFactory;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductRequestTest extends TestCase
@@ -37,5 +35,55 @@ class ProductRequestTest extends TestCase
         $this->actingAs($user);
         $response = $this->get(route('index'));
         $response->assertViewHas('isBirthday',false);
+    }
+
+    public function test_list_products()
+    {
+        $this->assertGuest();
+        $user = User::factory()->create([
+            'birthday' => Carbon::yesterday()
+        ]);
+        $this->actingAs($user);
+        $response = $this->get(route('index'));
+        $response->assertViewHas('products', function ($objects) {
+            foreach ($objects as $object){
+                if (!($object instanceof Product))
+                    return false;
+            }
+            return true;
+        });
+    }
+
+    public function test_discount_price_exists()
+    {
+        $user = User::factory()->create([
+            'birthday' => Carbon::today()
+        ]);
+        $this->actingAs($user);
+        $response = $this->get(route('index'));
+        $response->assertViewHas('products', function ($objects) {
+            foreach ($objects as $object){
+                if (!($object instanceof Product))
+                    return false;
+                if (!isset($object->oldPrice))
+                    return false;
+            }
+            return true;
+        });
+    }
+
+    public function test_discount_price_not_exists()
+    {
+        $this->assertGuest();
+        $response = $this->get(route('index'));
+        $response->assertViewHas('products', function ($objects) {
+            foreach ($objects as $object){
+                if (!($object instanceof Product))
+                    return false;
+                if (isset($object->oldPrice))
+                    return false;
+            }
+            return true;
+        });
     }
 }
